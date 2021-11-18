967b9e805219e22ae9f5bb3f883365a9918c143b
Yohann <yohann@blueboard.io>
2021-11-18 17:17:17 +0100

    Updating pr0nbot to handle age of empire 4 elo rating

--- pr0nbot/main.go
+++ pr0nbot/main.go
@@ -1,19 +1,19 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"math/rand"
 	"net/http"
 	"os"
-	"os/exec"
 	"os/signal"
 	"regexp"
 	"strconv"
 
-	// "strings"
 	"bytes"
 	"io/ioutil"
+	"strings"
 	"syscall"
 	"time"
 
@@ -61,71 +61,133 @@ type stop struct {
 }
 
 func main() {
-
-	// Create a new Discord session using the provided bot token.
 	dg, err := discordgo.New("Bot " + Token)
 	if err != nil {
 		fmt.Println("error creating Discord session,", err)
 		return
 	}
-
-	// Register the messageCreate func as a callback for MessageCreate events.
 	dg.AddHandler(messageCreate)
-
-	// Open a websocket connection to Discord and begin listening.
 	err = dg.Open()
 	if err != nil {
 		fmt.Println("error opening connection,", err)
 		return
 	}
-
-	// Wait here until CTRL-C or other term signal is received.
 	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
 	<-sc
-
-	// Cleanly close down the Discord session.
 	dg.Close()
 }
 
-func random(min, max int) int {
-	rand.Seed(time.Now().Unix())
-	return rand.Intn(max-min) + min
-}
+func getELOaoe4(s *discordgo.Session, m *discordgo.MessageCreate) {
 
-func getCmd() string {
-
-	myrand := strconv.Itoa(random(1, 2500))
-	cmd := `curl 'https://scrolller.com/api/media' -H 'origin: https://scrolller.com' -H 'accept-encoding: gzip, deflate, br' -H 'accept-language: en-US,en;q=0.9' -H 'user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.79 Safari/537.36' -H 'content-type: application/json' -H 'accept: */*' -H 'referer: https://scrolller.com/nsfw' -H 'authority: scrolller.com' -H 'cookie: _ga=GA1.2.661111315.1529359396; _gid=GA1.2.489592511.1529359396; _gat=1' --data-binary '[[` + myrand + `,null,0,2]]' --compressed`
-	return cmd
-}
-
-func sendpr0n(s *discordgo.Session, m *discordgo.MessageCreate) {
-
-	re := regexp.MustCompile(`([-a-zA-Z0-9_\/:.]+\.(jpg|mp4|webm))`)
-	chanName := regexp.MustCompile(`"(\w+)"`)
-	myrand := strconv.Itoa(random(1, 6000))
-	var jsonStr = []byte(`[[` + myrand + `,null,0,2]]`)
-	req, err := http.NewRequest("POST", "https://scrolller.com/api/media", bytes.NewBuffer(jsonStr))
-	if err != nil {
-		// handle err
+	type Payload struct {
+		Region       string `json:"region"`
+		Versus       string `json:"versus"`
+		MatchType    string `json:"matchType"`
+		TeamSize     string `json:"teamSize"`
+		SearchPlayer string `json:"searchPlayer"`
+		Page         int    `json:"page"`
+		Count        int    `json:"count"`
 	}
-	req.Header.Set("Authority", "scrolller.com")
-	req.Header.Set("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36")
+
+	type Results struct {
+		Count int `json:"count"`
+		Items []struct {
+			GameID       string      `json:"gameId"`
+			UserID       string      `json:"userId"`
+			RlUserID     int         `json:"rlUserId"`
+			UserName     string      `json:"userName"`
+			AvatarURL    interface{} `json:"avatarUrl"`
+			PlayerNumber interface{} `json:"playerNumber"`
+			Elo          int         `json:"elo"`
+			EloRating    int         `json:"eloRating"`
+			Rank         int         `json:"rank"`
+			Region       string      `json:"region"`
+			Wins         int         `json:"wins"`
+			WinPercent   float64     `json:"winPercent"`
+			Losses       int         `json:"losses"`
+			WinStreak    int         `json:"winStreak"`
+		} `json:"items"`
+	}
+
+	bodyMessage := strings.Fields(m.Content)
+	playerName := bodyMessage[1]
+	matchType := bodyMessage[2]
+
+	data := Payload{
+		Region:       "0",
+		Versus:       "players",
+		MatchType:    "unranked",
+		TeamSize:     matchType,
+		SearchPlayer: playerName,
+		Page:         1,
+		Count:        100,
+	}
+	payloadBytes, err := json.Marshal(data)
+	if err != nil {
+		fmt.Println(err)
+	}
+	body := bytes.NewReader(payloadBytes)
+
+	req, err := http.NewRequest("POST", "https://api.ageofempires.com/api/ageiv/Leaderboard", body)
+	if err != nil {
+		fmt.Println(err)
+	}
+	req.Header.Set("Authority", "api.ageofempires.com")
+	req.Header.Set("Sec-Ch-Ua", "\";Not A Brand\";v=\"99\", \"Chromium\";v=\"94\"")
+	req.Header.Set("Accept", "application/json, text/javascript, */*; q=0.01")
 	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Accept", "*/*")
-	req.Header.Set("Origin", "https://scrolller.com")
-	req.Header.Set("Sec-Fetch-Site", "same-origin")
+	req.Header.Set("Sec-Ch-Ua-Mobile", "?0")
+	req.Header.Set("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36")
+	req.Header.Set("Sec-Ch-Ua-Platform", "\"Linux\"")
+	req.Header.Set("Origin", "https://www.ageofempires.com")
+	req.Header.Set("Sec-Fetch-Site", "same-site")
 	req.Header.Set("Sec-Fetch-Mode", "cors")
 	req.Header.Set("Sec-Fetch-Dest", "empty")
-	req.Header.Set("Referer", "https://scrolller.com/nsfw")
+	req.Header.Set("Referer", "https://www.ageofempires.com/")
 	req.Header.Set("Accept-Language", "en-US,en;q=0.9,fr-FR;q=0.8,fr;q=0.7")
-	req.Header.Set("Cookie", "__cfduid=d282e4ea05bcbe9d31e8693b0901d45a51587755050; _ga=GA1.2.317970967.1587755062; _gid=GA1.2.1812126065.1588158281; _gat=1")
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		// handle err
+		fmt.Println(err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode == http.StatusOK {
+		var result Results
+		jsonBody, _ := ioutil.ReadAll(resp.Body)
+		err = json.Unmarshal(jsonBody, &result)
+		if err != nil {
+			fmt.Println(err)
+		}
+		s.ChannelMessageSend(m.ChannelID, "Elo: `"+strconv.Itoa(result.Items[0].Elo)+"` | Rank: `"+strconv.Itoa(result.Items[0].Rank)+"th`"+" | Winrate%: `"+strconv.Itoa(int(result.Items[0].WinPercent))+"`")
+	} else {
+		s.ChannelMessageSend(m.ChannelID, "No data found.")
+	}
+
+}
+
+func sendpr0n(s *discordgo.Session, m *discordgo.MessageCreate) {
+	re := regexp.MustCompile(`([-a-zA-Z0-9_\/:.]+\.(jpg|mp4|webm))`)
+	body := strings.NewReader("{\"query\":\" query DiscoverSubredditsQuery( $filter: MediaFilter $limit: Int $iterator: String $hostsDown: [HostDisk] ) { discoverSubreddits( isNsfw: true filter: $filter limit: $limit iterator: $iterator ) { iterator items { __typename url title secondaryTitle description createdAt isNsfw subscribers isComplete itemCount videoCount pictureCount albumCount isFollowing children( limit: 2 iterator: null filter: null disabledHosts: $hostsDown ) { iterator items { __typename url title subredditTitle subredditUrl redditPath isNsfw albumUrl isFavorite mediaSources { url width height isOptimized } } } } } } \",\"variables\":{\"limit\":30,\"filter\":null,\"hostsDown\":[\"NANO\",\"PICO\"]},\"authorization\":null}")
+	req, err := http.NewRequest("POST", "https://api.scrolller.com/api/v2/graphql", body)
+	if err != nil {
+		fmt.Println(err)
+	}
+	req.Header.Set("Authority", "api.scrolller.com")
+	req.Header.Set("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36")
+	req.Header.Set("Content-Type", "text/plain;charset=UTF-8")
+	req.Header.Set("Accept", "*/*")
+	req.Header.Set("Origin", "https://scrolller.com")
+	req.Header.Set("Sec-Fetch-Site", "same-site")
+	req.Header.Set("Sec-Fetch-Mode", "cors")
+	req.Header.Set("Sec-Fetch-Dest", "empty")
+	req.Header.Set("Referer", "https://scrolller.com/")
+	req.Header.Set("Accept-Language", "en-US,en;q=0.9,fr-FR;q=0.8,fr;q=0.7")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Println(err)
 	}
 	defer resp.Body.Close()
 	if resp.StatusCode == http.StatusOK {
@@ -134,86 +196,42 @@ func sendpr0n(s *discordgo.Session, m *discordgo.MessageCreate) {
 			fmt.Println(err)
 		}
 		bodyString := string(bodyBytes)
+
+		s.ChannelMessageSend(m.ChannelID, bodyString)
 		images := re.FindAllString(bodyString, -1)
 		if len(images) == 0 {
-			// restart func until we find an image
 			sendpr0n(s, m)
+			return
 		}
-		s.ChannelMessageSend(m.ChannelID, "Subreddit `"+chanName.FindString(bodyString)+"`")
-		s.ChannelMessageSend(m.ChannelID, "https://scrolller.com/media/"+images[0])
+		image := images[rand.Intn(len(images))]
+		resp, _ := http.Get(image)
+		if resp.StatusCode != http.StatusOK {
+			sendpr0n(s, m)
+			return
+		}
+		s.ChannelMessageSend(m.ChannelID, bodyString)
+		s.ChannelMessageSend(m.ChannelID, images[rand.Intn(len(images))])
+	} else {
+		s.ChannelMessageSend(m.ChannelID, strconv.Itoa(resp.StatusCode))
 	}
-
-	// grosseString := string(out)
-	// re := regexp.MustCompile(`([-a-zA-Z0-9_\/:.]+\.(jpg|mp4|webm))`)
-	// arrayDeString := re.FindAllString(grosseString, -1)
-	// if len(arrayDeString) == 0 {
-	// 	sendpr0n(s, m)
-	// }
-	// vals := arrayDeString
-	// r := rand.New(rand.NewSource(time.Now().Unix()))
-	// for _, i := range r.Perm(len(vals)) {
-	// 	val := vals[i]
-	// 	gfy := val
-	// 	reg := regexp.MustCompile(`gfy`)
-	// 	anus := reg.FindString(gfy)
-	// 	if anus == "gfy" {
-	// 		s.ChannelMessageSend(m.ChannelID, val)
-	// 		break
-	// 	}
-	// 	s.ChannelMessageSend(m.ChannelID, "https://scrolller.com/media/"+val)
-	// 	break
-	// }
-
 }
 
-func sendpr0nbomb(s *discordgo.Session, m *discordgo.MessageCreate) {
-
-	out, err := exec.Command("sh", "-c", getCmd()).Output()
-	if err != nil {
-		fmt.Println(err)
-	}
-	grosseString := string(out)
-	re := regexp.MustCompile(`([-a-zA-Z0-9_\/:.]+\.(jpg|mp4|webm))`)
-	arrayDeString := re.FindAllString(grosseString, -1)
-	if len(arrayDeString) == 0 {
-		sendpr0nbomb(s, m)
-	}
-
-	vals := arrayDeString
-	r := rand.New(rand.NewSource(time.Now().Unix()))
-	for _, i := range r.Perm(len(vals)) {
-		val := vals[i]
-		gfy := val
-		reg := regexp.MustCompile(`gfy`)
-		anus := reg.FindString(gfy)
-		if anus == "gfy" {
-			s.ChannelMessageSend(m.ChannelID, val)
-			break
-		}
-		s.ChannelMessageSend(m.ChannelID, "https://scrolller.com/media/"+val)
-		if i == 5 {
-			break
-		}
-	}
-
-}
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-
-	// Ignore all messages created by the bot itself
-	// This isn't required in this specific example but it's a good practice.
 	if m.Author.ID == s.State.User.ID {
 		return
 	}
-
-	// If the message is "ping" reply with "Pong!"
+	r, _ := regexp.Compile(".elo ")
+	msg := r.FindString(m.Content)
+	if msg != "" {
+		getELOaoe4(s, m)
+	}
 	if m.Content == ".pr0n" {
 		sendpr0n(s, m)
 	}
-	if m.Content == ".pr0nbomb" {
-		sendpr0nbomb(s, m)
+	if m.Content == ".helpzer" {
+		s.ChannelMessageSend(m.ChannelID, "``` .pr0n | .elo playerName matchType | .kathi0u | .helpzer ```")
 	}
-	if m.Content == ".ocl0cks" {
-		s.ChannelMessageSend(m.ChannelID, "https://slacker.scrolller.com/media/38da5d.mp4")
+	if m.Content == ".kathi0u" {
+		s.ChannelMessageSend(m.ChannelID, "https://cdn.discordapp.com/attachments/633980782175584256/673619354360741912/kat.gif")
 	}
-
 }
