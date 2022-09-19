3b4709135fe38f2b4c42f2f98fbd44cf89d8574f
kathiouchka <yohann.beaure@gmail.com>
2022-09-19 19:19:49 +0200

    fix: now excluding redfigs

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"fmt"
 	"math/rand"
@@ -11,7 +10,6 @@ import (
 	"regexp"
 	"strconv"
 
-	"bytes"
 	"io/ioutil"
 	"strings"
 	"syscall"
@@ -79,99 +77,99 @@ func main() {
 	dg.Close()
 }
 
-func getELOaoe4(s *discordgo.Session, m *discordgo.MessageCreate) {
+// func getELOaoe4(s *discordgo.Session, m *discordgo.MessageCreate) {
 
-	type Payload struct {
-		Region       string `json:"region"`
-		Versus       string `json:"versus"`
-		MatchType    string `json:"matchType"`
-		TeamSize     string `json:"teamSize"`
-		SearchPlayer string `json:"searchPlayer"`
-		Page         int    `json:"page"`
-		Count        int    `json:"count"`
-	}
+// 	type Payload struct {
+// 		Region       string `json:"region"`
+// 		Versus       string `json:"versus"`
+// 		MatchType    string `json:"matchType"`
+// 		TeamSize     string `json:"teamSize"`
+// 		SearchPlayer string `json:"searchPlayer"`
+// 		Page         int    `json:"page"`
+// 		Count        int    `json:"count"`
+// 	}
 
-	type Results struct {
-		Count int `json:"count"`
-		Items []struct {
-			GameID       string      `json:"gameId"`
-			UserID       string      `json:"userId"`
-			RlUserID     int         `json:"rlUserId"`
-			UserName     string      `json:"userName"`
-			AvatarURL    interface{} `json:"avatarUrl"`
-			PlayerNumber interface{} `json:"playerNumber"`
-			Elo          int         `json:"elo"`
-			EloRating    int         `json:"eloRating"`
-			Rank         int         `json:"rank"`
-			Region       string      `json:"region"`
-			Wins         int         `json:"wins"`
-			WinPercent   float64     `json:"winPercent"`
-			Losses       int         `json:"losses"`
-			WinStreak    int         `json:"winStreak"`
-		} `json:"items"`
-	}
+// 	type Results struct {
+// 		Count int `json:"count"`
+// 		Items []struct {
+// 			GameID       string      `json:"gameId"`
+// 			UserID       string      `json:"userId"`
+// 			RlUserID     int         `json:"rlUserId"`
+// 			UserName     string      `json:"userName"`
+// 			AvatarURL    interface{} `json:"avatarUrl"`
+// 			PlayerNumber interface{} `json:"playerNumber"`
+// 			Elo          int         `json:"elo"`
+// 			EloRating    int         `json:"eloRating"`
+// 			Rank         int         `json:"rank"`
+// 			Region       string      `json:"region"`
+// 			Wins         int         `json:"wins"`
+// 			WinPercent   float64     `json:"winPercent"`
+// 			Losses       int         `json:"losses"`
+// 			WinStreak    int         `json:"winStreak"`
+// 		} `json:"items"`
+// 	}
 
-	bodyMessage := strings.Fields(m.Content)
-	var playerName string
-	var matchType string
-	if len(bodyMessage) != 3 {
-		s.ChannelMessageSend(m.ChannelID, "Wrong entry. Example : `.elo Kathiou 1v1`")
-		return
-	}
-	playerName = bodyMessage[1]
-	matchType = bodyMessage[2]
+// 	bodyMessage := strings.Fields(m.Content)
+// 	var playerName string
+// 	var matchType string
+// 	if len(bodyMessage) != 3 {
+// 		s.ChannelMessageSend(m.ChannelID, "Wrong entry. Example : `.elo Kathiou 1v1`")
+// 		return
+// 	}
+// 	playerName = bodyMessage[1]
+// 	matchType = bodyMessage[2]
 
-	data := Payload{
-		Region:       "0",
-		Versus:       "players",
-		MatchType:    "unranked",
-		TeamSize:     matchType,
-		SearchPlayer: playerName,
-		Page:         1,
-		Count:        100,
-	}
-	payloadBytes, err := json.Marshal(data)
-	if err != nil {
-		fmt.Println(err)
-	}
-	body := bytes.NewReader(payloadBytes)
+// 	data := Payload{
+// 		Region:       "0",
+// 		Versus:       "players",
+// 		MatchType:    "unranked",
+// 		TeamSize:     matchType,
+// 		SearchPlayer: playerName,
+// 		Page:         1,
+// 		Count:        100,
+// 	}
+// 	payloadBytes, err := json.Marshal(data)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
+// 	body := bytes.NewReader(payloadBytes)
 
-	req, err := http.NewRequest("POST", "https://api.ageofempires.com/api/ageiv/Leaderboard", body)
-	if err != nil {
-		fmt.Println(err)
-	}
-	req.Header.Set("Authority", "api.ageofempires.com")
-	req.Header.Set("Sec-Ch-Ua", "\";Not A Brand\";v=\"99\", \"Chromium\";v=\"94\"")
-	req.Header.Set("Accept", "application/json, text/javascript, */*; q=0.01")
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Sec-Ch-Ua-Mobile", "?0")
-	req.Header.Set("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36")
-	req.Header.Set("Sec-Ch-Ua-Platform", "\"Linux\"")
-	req.Header.Set("Origin", "https://www.ageofempires.com")
-	req.Header.Set("Sec-Fetch-Site", "same-site")
-	req.Header.Set("Sec-Fetch-Mode", "cors")
-	req.Header.Set("Sec-Fetch-Dest", "empty")
-	req.Header.Set("Referer", "https://www.ageofempires.com/")
-	req.Header.Set("Accept-Language", "en-US,en;q=0.9,fr-FR;q=0.8,fr;q=0.7")
+// 	req, err := http.NewRequest("POST", "https://api.ageofempires.com/api/ageiv/Leaderboard", body)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
+// 	req.Header.Set("Authority", "api.ageofempires.com")
+// 	req.Header.Set("Sec-Ch-Ua", "\";Not A Brand\";v=\"99\", \"Chromium\";v=\"94\"")
+// 	req.Header.Set("Accept", "application/json, text/javascript, */*; q=0.01")
+// 	req.Header.Set("Content-Type", "application/json")
+// 	req.Header.Set("Sec-Ch-Ua-Mobile", "?0")
+// 	req.Header.Set("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36")
+// 	req.Header.Set("Sec-Ch-Ua-Platform", "\"Linux\"")
+// 	req.Header.Set("Origin", "https://www.ageofempires.com")
+// 	req.Header.Set("Sec-Fetch-Site", "same-site")
+// 	req.Header.Set("Sec-Fetch-Mode", "cors")
+// 	req.Header.Set("Sec-Fetch-Dest", "empty")
+// 	req.Header.Set("Referer", "https://www.ageofempires.com/")
+// 	req.Header.Set("Accept-Language", "en-US,en;q=0.9,fr-FR;q=0.8,fr;q=0.7")
 
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer resp.Body.Close()
-	if resp.StatusCode == http.StatusOK {
-		var result Results
-		jsonBody, _ := ioutil.ReadAll(resp.Body)
-		err = json.Unmarshal(jsonBody, &result)
-		if err != nil {
-			fmt.Println(err)
-		}
-		s.ChannelMessageSend(m.ChannelID, "Elo: `"+strconv.Itoa(result.Items[0].Elo)+"` | Rank: `"+strconv.Itoa(result.Items[0].Rank)+"th`"+" | Winrate%: `"+strconv.Itoa(int(result.Items[0].WinPercent))+"`"+"| Winstreak: `"+strconv.Itoa(int(result.Items[0].WinStreak))+"`")
-	} else {
-		s.ChannelMessageSend(m.ChannelID, "No data found.")
-	}
+// 	resp, err := http.DefaultClient.Do(req)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
+// 	defer resp.Body.Close()
+// 	if resp.StatusCode == http.StatusOK {
+// 		var result Results
+// 		jsonBody, _ := ioutil.ReadAll(resp.Body)
+// 		err = json.Unmarshal(jsonBody, &result)
+// 		if err != nil {
+// 			fmt.Println(err)
+// 		}
+// 		s.ChannelMessageSend(m.ChannelID, "Elo: `"+strconv.Itoa(result.Items[0].Elo)+"` | Rank: `"+strconv.Itoa(result.Items[0].Rank)+"th`"+" | Winrate%: `"+strconv.Itoa(int(result.Items[0].WinPercent))+"`"+"| Winstreak: `"+strconv.Itoa(int(result.Items[0].WinStreak))+"`")
+// 	} else {
+// 		s.ChannelMessageSend(m.ChannelID, "No data found.")
+// 	}
 
-}
+// }
 
 // global historic of 10 last message from discord bot
 var historic [10]string
@@ -209,7 +207,8 @@ func remAllFromHistory(s *discordgo.Session, m *discordgo.MessageCreate) {
 func sendpr0n(s *discordgo.Session, m *discordgo.MessageCreate) {
 	re := regexp.MustCompile(`([-a-zA-Z0-9_\/:.]+(360).(jpg))`)
 	if strings.Contains(m.Content, "vid") {
-		re = regexp.MustCompile(`([-a-zA-Z0-9_\/:.]+.(mp4))`)
+		// re = regexp.MustCompile(`([-a-zA-Z0-9_\/:.]+.(mp4))`)
+		re = regexp.MustCompile(`(https://[-a-zA-Z0-9]+.scrolller.com/[-a-zA-Z0-9]+.mp4)`)
 	}
 	body := strings.NewReader("{\"query\":\" query DiscoverSubredditsQuery( $filter: MediaFilter $limit: Int $iterator: String $hostsDown: [HostDisk] ) { discoverSubreddits( isNsfw: true filter: $filter limit: $limit iterator: $iterator ) { iterator items { __typename url title secondaryTitle description createdAt isNsfw subscribers isComplete itemCount videoCount pictureCount albumCount isFollowing children( limit: 2 iterator: null filter: null disabledHosts: $hostsDown ) { iterator items { __typename url title subredditTitle subredditUrl redditPath isNsfw albumUrl isFavorite mediaSources { url width height isOptimized } } } } } } \",\"variables\":{\"limit\":30,\"filter\":null,\"hostsDown\":[\"NANO\",\"PICO\"]},\"authorization\":null}")
 	req, err := http.NewRequest("POST", "https://api.scrolller.com/api/v2/graphql", body)
@@ -238,12 +237,13 @@ func sendpr0n(s *discordgo.Session, m *discordgo.MessageCreate) {
 			fmt.Println(err)
 		}
 		bodyString := string(bodyBytes)
-		images := re.FindAllString(bodyString, -1)
-		if len(images) == 0 {
+		urls := re.FindAllString(bodyString, -1)
+		fmt.Println(urls)
+		if len(urls) == 0 {
 			sendpr0n(s, m)
 		}
-		image := images[rand.Intn(len(images))]
-		s.ChannelMessageSend(m.ChannelID, image)
+		urlToSend := urls[rand.Intn(len(urls))]
+		s.ChannelMessageSend(m.ChannelID, urlToSend)
 		addToHistory(s, m)
 
 	} else {
@@ -255,16 +255,12 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	if m.Author.ID == s.State.User.ID {
 		return
 	}
-	r, _ := regexp.Compile("\\.elo ")
-	msg := r.FindString(m.Content)
-	if msg != "" {
-		getELOaoe4(s, m)
-	}
+
 	if m.Content == ".pr0n" || m.Content == ".pr0n vid" {
 		sendpr0n(s, m)
 	}
 	if m.Content == ".helpzer" {
-		s.ChannelMessageSend(m.ChannelID, "``` .pr0n | .elo playerName matchType | .kathi0u | .helpzer ```")
+		s.ChannelMessageSend(m.ChannelID, "``` .pr0n | .pr0n vid | .kathi0u | .helpzer | kathioubob ```")
 	}
 	regexDeRebeu, _ := regexp.Compile("(?i)arabe")
 	if regexDeRebeu.MatchString(m.Content) {
