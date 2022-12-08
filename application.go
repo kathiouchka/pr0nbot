dd8c86c8d822991f7a77062cf3c36ea578f4fd13
kathiouchka <yohann.beaure@gmail.com>
2022-12-08 16:02:50 +0100

    fix: print nothing

--- application.go
+++ application.go
@@ -40,7 +40,7 @@ func main() {
 		fmt.Println("error opening connection,", err)
 		return
 	}
-	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+	// fmt.Println("Bot is now running.  Press CTRL-C to exit.")
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
 	<-sc
