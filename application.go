ee041aa8fbebea56b4156ea288933dba4078569f
kathiouchka <yohann.beaure@gmail.com>
2022-12-08 17:47:55 +0100

    fix: jpg all sizes

--- application.go
+++ application.go
@@ -88,7 +88,8 @@ func remAllFromHistory(s *discordgo.Session, m *discordgo.MessageCreate) {
 
 func sendpr0n(s *discordgo.Session, m *discordgo.MessageCreate) {
 
-	re := regexp.MustCompile(`[-a-zA-Z0-9_/:.]+(360).(jpg)`)
+	// re := regexp.MustCompile(`[-a-zA-Z0-9_/:.]+(360).(jpg)`)
+	re := regexp.MustCompile(`[-a-zA-Z0-9_/:.]+(jpg)`)
 	// re := regexp.MustCompile(`[-a-zA-Z0-9_/:.]`)
 	if strings.Contains(m.Content, "vid") {
 		re = regexp.MustCompile(`https://[-a-zA-Z0-9]+.scrolller.com/[-a-zA-Z0-9]+.mp4`)
